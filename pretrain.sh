d8a17144efb3c019a364c2d266bf508570e2aa14
zichunyu <zichunyu@devgpu174.cln3.facebook.com>
2024-06-03 15:41:59 -0700

    Init MATES

--- pretrain.sh
+++ pretrain.sh
@@ -1,11 +1,13 @@
-export WANDB_API_KEY=bda3630516768ce4285b4d8205dd6c0189520be1
+export WANDB_API_KEY=
 
 python -m litgpt pretrain \
-  --model_name pythia-160m \
+  --model_name pythia-410m \
   --tokenizer_dir checkpoints/EleutherAI/pythia-160m \
   --data FineWeb \
-  --train.micro_batch_size 32 \
-  --train.max_tokens 10_000_000_000 \
-  --out_dir /data/users/zichunyu/out/pythia-160m-ddp/fineweb/sample-10BT \
+  --train.save_interval 5000 \
+  --train.micro_batch_size 16 \
+  --train.max_tokens 50_000_000_000 \
+  --eval.interval 200000 \
+  --out_dir /data/users/zichunyu/out/pythia-410m/fineweb/sample-100BT \
   --logger_name wandb \
-  --exp_name pythia-160m_fineweb_sample-10BT-ddp
+  --exp_name pythia-410m_fineweb_sample-100BT
